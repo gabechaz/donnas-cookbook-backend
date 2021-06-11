cb0a2dce173e8450f285bc3991620df3980045c8
Gabriel Chazanov <gabe.chaz@gmail.com>
2021-06-11 15:53:27 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,26 @@
 Rails.application.routes.draw do
-  resources :foods
-  resources :ingredients
-  resources :recipes
-  resources :users
+  # resources :foods
+  # resources :ingredients
+  # resources :recipes
+  # resources :users
+
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  post '/users', to: 'users#create'
+
+  get '/foods', to: 'foods#index'
+  get '/foods/:id', to: 'foods#show'
+  post '/foods', toL 'foods#create'
+
+  get '/recipes', to: 'recipes#index'
+  get '/recipes/:id', to: 'recipes#show'
+  post '/recipes', to: 'recipes#create'
+
+  get '/ingredients', to: 'ingredients#index'
+  get '/ingredients/:id'
+  post '/ingredients', to: 'ingredients#'
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
