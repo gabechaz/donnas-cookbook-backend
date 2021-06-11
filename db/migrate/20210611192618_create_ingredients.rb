84401353813b727a408b3f5cfb19a1a44896515b
Gabriel Chazanov <gabe.chaz@gmail.com>
2021-06-11 15:37:38 -0400

    added models and migrations

--- db/migrate/20210611192618_create_ingredients.rb
+++ db/migrate/20210611192618_create_ingredients.rb
@@ -2,8 +2,8 @@ class CreateIngredients < ActiveRecord::Migration[6.0]
   def change
     create_table :ingredients do |t|
       t.string name
-      t.integer food_id
-      t.integer recipe_id
+      t.integer :food_id
+      t.integer :recipe_id
       t.timestamps
     end
   end
